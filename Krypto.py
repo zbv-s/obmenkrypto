331151f600efd0ad4ef7f68144f00f1c1aaf45a2
zbv-s <aleksandra.jargalova@gmail.com>
2024-08-08 13:37:54 +0800

    функция для обмена курса валют из инета

--- Krypto.py
+++ Krypto.py
@@ -5,6 +5,23 @@ from tkinter import messagebox as mb
 
 
 def exchange():
+    code = entry.get()
+
+    if code:
+        try:
+            response = requests.get('https://open.er-api.com/v6/latest/USD')
+            response.raise_for_status()
+            data = response.json()
+            if code in data['rates']:
+                exchange_rate = data['rates'][code]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate} {code} за 1 доллар")
+            else:
+                mb.showerror("Ошибка", f"Валюта {code} не найдена!")
+        except Exception as e:
+            mb.showerror("Ошибка", f"Произошла ошибка: {e}.")
+    else:
+        mb.showwarning("Внимание!", "Введите код валюты!")
+
 
 
 
@@ -22,7 +39,3 @@ Button(text="Получить курс обмена к доллару", command=
 window.mainloop()
 
 
-result = requests.get('https://open.er-api.com/v6/latest/USD')
-data = json.loads(result.text)
-p = pprint.PrettyPrinter(indent=4)
-p.pprint(data)
